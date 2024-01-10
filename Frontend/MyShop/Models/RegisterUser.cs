a7369feb3e071a267c8d799a87e7832451d3374b
Kelvin Ngeiywa <ngeiywakelvin05@gmail.com>
2024-01-10 12:26:19 +0300

    Ecommerce project

---
+++ Frontend/MyShop/Models/RegisterUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyShop;
+
+public class RegisterUser
+{
+    [Required]
+    public string Name {get;set;}="";
+    [Required]
+    [EmailAddress]
+    public string Email {get;set;}="";
+    [Required]
+    public string Password {get;set;}="";
+
+}
---
+++ Frontend/MyShopUi/Models/RegisterUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyShopUi.Models;
+
+public class RegisterUser
+{
+     [Required]
+    public string Name {get;set;}="";
+    [Required]
+    [EmailAddress]
+    public string Email {get;set;}="";
+    [Required]
+    public string Password {get;set;}="";
+
+}
